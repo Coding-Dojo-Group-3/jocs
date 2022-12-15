a97f2a94ca9ac1195db902ab213a9d1513569a7b
Charlie <c.alejandro.oso@gmail.com>
2022-12-15 12:23:37 -0800

    added redux and set up reducer for user state. Also made a blank dashboard view and set up the route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+import Dashboard from './views/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          {/* Home */}
+          <Route path="/" element= {<Navigate to="/dashboard" />} />
+
+          {/* Create and Read */}
+          <Route path="/dashboard" element={<Dashboard/>} />
+
+          {/* Update */}
+          
+          {/* Not Found */}
+          {/* <Route path="" element={<NotFound/>}/> */}
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
